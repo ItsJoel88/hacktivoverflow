c36854512fd0a84d81838b27fcc92e061b804fda
ItsJoel88 <imanuelnjodi@gmail.com>
2019-11-22 09:02:30 +0700

    .

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,10 @@ const UserRouter = require('./user')
 const ArticleRouter = require('./article')
 const CommentRouter = require('./comment')
 
+router.get('/', (req, res, next) => {
+    res.json({ message: 'Hello' })
+})
+
 router.use('/articles', ArticleRouter)
 
 router.use('/users', UserRouter)
