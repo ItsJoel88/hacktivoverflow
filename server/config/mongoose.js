909574d13807883e6e3a1af5f935462b75b245f5
ItsJoel88 <imanuelnjodi@gmail.com>
2020-01-23 11:02:28 +0000

    changes from ssh

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 // process.env.MONGO_URI
 module.exports = () => {
-    mongoose.connect('mongodb://localhost/hacktivoverflow'
+    mongoose.connect(process.env.MONGO_URI
         , { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
-            console.log('connected to database')
+		if(err) console.log(err)
+            else console.log('connected to database')
         })
 }
